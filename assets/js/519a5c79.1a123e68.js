2dd63078b8bdbd4e28f415abeb39b6989b57acb9
pradip-valens <pradip.sodha@rackspace.com>
2023-12-08 16:06:12 +0530

    Deploy website - based on 2d9700e5b9e5849095fb059782850d387c640cc3

---
+++ assets/js/519a5c79.1a123e68.js
@@ -0,0 +1,1 @@
+(self.webpackChunkguzzle=self.webpackChunkguzzle||[]).push([[8770],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
